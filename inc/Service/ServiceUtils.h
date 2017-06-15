f6e2d09867e997c57f089e70080c69e19dcbbd0a
huangqiwei <dz2008hqw@163.com>
2017-06-15 11:15:25 +0800

    实现 ServiceInstall 和 ServiceUninstall 接口

--- inc/Service/ServiceUtils.h
+++ inc/Service/ServiceUtils.h
@@ -49,6 +49,23 @@ typedef struct
 */
 DLL_COMMONLIB_API int WINAPI ServiceMain(ServiceBaseInfo *ServiceInfo);
 
+/** @brief 获取服务状态
+*	@param[in]	serviceName	服务名称
+*	@param[out] dwState 服务状态
+*	@dwState 返回值列表
+*		SERVICE_STOPPED
+*		SERVICE_START_PENDING
+*		SERVICE_STOP_PENDING
+*		SERVICE_RUNNING
+*		SERVICE_CONTINUE_PENDING
+*		SERVICE_PAUSE_PENDING
+*		SERVICE_PAUSED
+*	@return  返回操作结果
+*	 - false 失败
+*	 - true 成功
+*/
+DLL_COMMONLIB_API BOOL WINAPI GetServiceState(const WCHAR* serviceName, DWORD* dwState);
+
 /** @brief 启动服务
 *	@param[in]	serviceName	服务名称
 *	@return  返回操作结果
@@ -64,6 +81,24 @@ DLL_COMMONLIB_API BOOL WINAPI ServiceStart(const WCHAR* serviceName);
 *	 - true 成功
 */
 DLL_COMMONLIB_API BOOL WINAPI ServiceStop(const WCHAR* serviceName);
+
+/** @brief 安装服务，服务类型为自动运行
+*	@param[in]	binPath 服务主程序完整路径
+*	@param[in]	serviceName 服务名称
+*	@param[in]	description 服务描述
+*	@return  返回操作结果
+*	 - false 失败
+*	 - true 成功
+*/
+DLL_COMMONLIB_API BOOL WINAPI ServiceInstall(const WCHAR* binPath, const WCHAR* serviceName, WCHAR* description);
+
+/** @brief 卸载服务
+*	@param[in]	serviceName 服务名称
+*	@return  返回操作结果
+*	 - false 失败
+*	 - true 成功
+*/
+DLL_COMMONLIB_API BOOL WINAPI ServiceUninstall(const WCHAR* serviceName);
 
 #ifdef __cplusplus
 }
--- src/Service/ServiceUtils.cpp
+++ src/Service/ServiceUtils.cpp
@@ -12,6 +12,48 @@ int WINAPI ServiceMain(ServiceBaseInfo *ServiceInfo)
 	return ServiceMainLib(ServiceInfo);
 }
 
+BOOL WINAPI GetServiceState(const WCHAR* serviceName, DWORD* dwState)
+{
+	SC_HANDLE scm, service;
+	SERVICE_STATUS serviceStatus;
+	*dwState = 0;
+
+	L_TRACE_ENTER();
+	L_INFO(L"GetServiceState begin, name = %s\r\n", serviceName);
+
+	scm = OpenSCManager(0, 0, SC_MANAGER_CONNECT);
+	if (!scm)
+	{
+		L_FATAL(L"OpenSCManager Fail(%d)\r\n", GetLastError());
+		return FALSE;
+	}
+
+	service = OpenService(scm, serviceName, SERVICE_QUERY_STATUS);
+	if (!service)
+	{
+		L_FATAL(L"OpenService Fail(%d)\r\n", GetLastError());
+		CloseServiceHandle(scm);
+		return FALSE;
+	}
+
+	if (!QueryServiceStatus(service, &serviceStatus))
+	{
+		L_FATAL(L"QueryServiceStatus Fail(%d)\r\n", GetLastError());
+		CloseServiceHandle(service);
+		CloseServiceHandle(scm);
+		return FALSE;
+	}
+
+	CloseServiceHandle(service);
+	CloseServiceHandle(scm);
+
+	L_INFO(L"GetServiceState success, name = %s\r\n", serviceName);
+	L_TRACE_LEAVE();
+
+	*dwState = serviceStatus.dwCurrentState;
+	return TRUE;
+}
+
 BOOL WINAPI ServiceStart(const WCHAR* serviceName)
 {
 	SC_HANDLE scm, service;
@@ -34,6 +76,21 @@ BOOL WINAPI ServiceStart(const WCHAR* serviceName)
 		L_FATAL(L"OpenService Fail(%d)\r\n", GetLastError());
 		CloseServiceHandle(scm);
 		return FALSE;
+	}
+
+	if (!QueryServiceStatus(service, &serviceStatus))
+	{
+		L_FATAL(L"QueryServiceStatus Fail(%d)\r\n", GetLastError());
+		CloseServiceHandle(service);
+		CloseServiceHandle(scm);
+		return FALSE;
+	}
+	if (serviceStatus.dwCurrentState == SERVICE_RUNNING)
+	{
+		L_INFO(L"Service has been running already, name = %s\r\n", serviceName);
+		CloseServiceHandle(service);
+		CloseServiceHandle(scm);
+		return TRUE;
 	}
 
 	if (!StartService(service, 0, NULL))
@@ -139,6 +196,129 @@ BOOL WINAPI ServiceStop(const WCHAR* serviceName)
 	CloseServiceHandle(scm);
 
 	L_INFO(L"Stop service success, name = %s\r\n", serviceName);
+	L_TRACE_LEAVE();
+
+	return TRUE;
+}
+
+BOOL WINAPI ServiceInstall(const WCHAR* binPath, const WCHAR* serviceName, WCHAR* description)
+{
+	SC_HANDLE scm, service;
+	SERVICE_DESCRIPTION sd;
+
+	L_TRACE_ENTER();
+	L_INFO(L"Install service begin, name = %s\r\n", serviceName);
+
+	scm = OpenSCManager(0, 0, SC_MANAGER_CREATE_SERVICE);
+	if (!scm)
+	{
+		L_FATAL(L"OpenSCManager Fail(%d)\r\n", GetLastError());
+		return FALSE;
+	}
+
+	service = CreateService(scm,
+		serviceName,
+		serviceName,
+		SERVICE_ALL_ACCESS,
+		SERVICE_WIN32_OWN_PROCESS | SERVICE_INTERACTIVE_PROCESS,
+		SERVICE_AUTO_START,
+		SERVICE_ERROR_NORMAL,
+		binPath,
+		NULL, NULL, NULL, NULL, NULL);
+	if (!service)
+	{
+		if (ERROR_SERVICE_EXISTS == GetLastError())	//已经存在该服务名称了
+		{
+			L_INFO(L"Service has been exist already, name = %s", serviceName);
+			service = OpenService(scm,
+				serviceName,
+				SERVICE_QUERY_STATUS |
+				SERVICE_START |
+				SERVICE_CHANGE_CONFIG |
+				SERVICE_QUERY_CONFIG);
+		}
+	}
+	else
+	{
+		//设置服务的描述信息
+		if (NULL != description)
+		{
+			sd.lpDescription = description;
+			ChangeServiceConfig2(service, SERVICE_CONFIG_DESCRIPTION, &sd);
+		}
+	}
+
+	if (!service)
+	{
+		L_FATAL(L"CreateService Fail(%d)\r\n", GetLastError());
+		CloseServiceHandle(scm);
+		return FALSE;
+	}
+
+	CloseServiceHandle(service);
+	CloseServiceHandle(scm);
+	
+	L_INFO(L"Install service success, name = %s\r\n", serviceName);
+	L_TRACE_LEAVE();
+
+	return TRUE;
+}
+
+BOOL WINAPI ServiceUninstall(const WCHAR* serviceName)
+{
+	SC_HANDLE scm, service;
+	SERVICE_STATUS serviceStatus;
+
+	L_TRACE_ENTER();
+	L_INFO(L"Uninstall service begin, name = %s\r\n", serviceName);
+
+	scm = OpenSCManager(0, 0, SC_MANAGER_CONNECT);
+	if (!scm)
+	{
+		L_FATAL(L"OpenSCManager Fail(%d)\r\n", GetLastError());
+		return FALSE;
+	}
+
+	service = OpenService(scm, serviceName, SERVICE_QUERY_STATUS | SERVICE_STOP | DELETE);
+	if (!service)
+	{
+		L_FATAL(L"OpenService Fail(%d)\r\n", GetLastError());
+		CloseServiceHandle(scm);
+		return FALSE;
+	}
+
+	if (!QueryServiceStatus(service, &serviceStatus))
+	{
+		L_FATAL(L"QueryServiceStatus Fail(%d)\r\n", GetLastError());
+		CloseServiceHandle(service);
+		CloseServiceHandle(scm);
+		return FALSE;
+	}
+
+	if (serviceStatus.dwCurrentState != SERVICE_STOPPED)
+	{
+		BOOL bStop = ServiceStop(serviceName);
+		if (!bStop)
+		{
+			L_FATAL(L"ServiceStop Fail(%d)\r\n", GetLastError());
+			CloseServiceHandle(service);
+			CloseServiceHandle(scm);
+			return FALSE;
+		}
+	}
+
+	if (!DeleteService(service))
+	{
+		L_FATAL(L"DeleteService Fail(%d)\r\n", GetLastError());
+		CloseServiceHandle(service);
+		CloseServiceHandle(scm);
+		return FALSE;
+	}
+
+	CloseServiceHandle(service);
+	CloseServiceHandle(scm);
+
+	L_INFO(L"Uninstall service success, name = %s\r\n", serviceName);
 	L_TRACE_LEAVE();
 
 	return TRUE;
